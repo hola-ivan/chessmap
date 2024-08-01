6565539621d85b90313164bdfbaff923fd442505
ier <example@mail.com>
2024-08-01 13:25:32 +0200

    Included vercel analytics

--- api/translate.js
+++ api/translate.js
@@ -1,9 +1,11 @@
 // api/translate.js
-
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
     const { text, targetLang } = req.query;
+
+    console.log('Detected text:', text); // Logging text
+    console.log('Detected target language:', targetLang); // Logging target language
 
     if (!text || !targetLang) {
         return res.status(400).json({ error: 'Missing text or targetLang parameter' });
@@ -14,6 +16,8 @@ module.exports = async (req, res) => {
     try {
         const response = await fetch(`https://api-free.deepl.com/v2/translate?auth_key=${apiKey}&text=${encodeURIComponent(text)}&target_lang=${targetLang}`);
         const data = await response.json();
+
+        console.log('Deepl Response:', data); // Logging Deepl response
 
         if (response.ok) {
             res.status(200).json({ translation: data.translations[0].text });
